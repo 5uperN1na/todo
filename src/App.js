4f76ea5fe3a6de913fe729fafe1b04fc28f42d68
Paula Suarez <paula.suarez@freeman.com>
2020-04-06 11:24:51 -0500

    Adding updates to repo

--- src/App.js
+++ src/App.js
@@ -1,37 +1,58 @@
 import React, { Component } from 'react';
+import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import './App.css';
+
 
 class App extends Component {
   state = {
     todos: [
       {
         id: 1,
-        task: "Journal",
+        title: "Journal",
         completed: false
       },
       {
         id: 2,
-        task: "Workout",
-        completed: false
+        title: "Workout",
+        completed: true
       },
       {
         id: 3,
-        task: "Yoga",
+        title: "Yoga",
         completed: false
       },
 
     ]
   }
 
+  //Toggle complete
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo;
+      })
+    });
+  }
+
+  //Delete Todo
+  delTodo = (id)=>{
+    this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)]})
+  }
+
   render() {
     console.log(this.state.todos);
     return (
-      <div className="App"> 
-      <Todos todos={this.state.todos} />
+      <div className="App">
+        <Header />
+        <Todos todos={this.state.todos} markComplete={this.markComplete}
+        delTodo={this.delTodo}/>
       </div>
     );
   }
 }
 
-  export default App;
+export default App;
